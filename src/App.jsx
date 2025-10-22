dfc26b937b335ba24dcd182bb153ddbfc4baaf3d
Anton Nageh <anton.nageh20@gmail.com>
2025-10-22 16:55:14 +0300

    Fixed vercel problem

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,6 @@ const App = () => (
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
